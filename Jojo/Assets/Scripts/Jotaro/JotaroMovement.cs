0fe5e54d8b36a26a47aab1ca209e92947325e44f
Randy Nguyen <randyvan12@gmail.com>
2020-09-24 20:10:53 -0500

    Jotaro Movement

--- Jojo/Assets/Scripts/Jotaro/JotaroMovement.cs
+++ Jojo/Assets/Scripts/Jotaro/JotaroMovement.cs
@@ -2,17 +2,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class JotaroScript : MonoBehaviour
+public class JotaroMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float movementSpeed;
+    public Rigidbody2D rb;
+    float mx;
+
+    public float jumpForce;
+    public Transform feet;
+    public LayerMask groundLayers;
+
+    private void Update()
     {
-        
+        mx = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetButtonDown("Jump") && isGrounded())
+        {
+            Jump();
+        }
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        Vector2 movement = new Vector2(mx * movementSpeed, rb.velocity.y);
+
+        rb.velocity = movement;
+    }
+
+    void Jump()
+    {
+        Vector2 movement = new Vector2(rb.velocity.x, jumpForce);
+
+        rb.velocity = movement;
+    }
+
+    public bool isGrounded()
+    {
+        Collider2D groundCheck = Physics2D.OverlapCircle(feet.position, 0.5f, groundLayers);
+
+        if (groundCheck != null)
+        {
+            return true;
+        }
+        return false;
     }
 }
